3920f21cb691f1843336f046313a60cf4afb485f
Mary Gibbs <mgibbs1259@gmail.com>
2020-05-11 19:47:34 -0400

    minor refactor

--- app.py
+++ app.py
@@ -11,24 +11,25 @@ def spotihue_welcome():
 
 @app.route("/connect", methods=["GET", "POST"])
 def spotihue_connect():
-    if request.form["submit_button"] == "Yes":
-        try:
-            spotihue = SpotiHue()
-            spotihue.connect_hue_bridge_first_time()
-            return render_template("spotihue_main.html")
-        except:
-            return render_template("spotihue_welcome.html", message='Connection unsuccessful. Please press Hue Bridge '
-                                                                    'button and click/press "Yes".')
-    else:
-        return render_template("spotihue_main.html")
+    if request.method == "POST":
+        if request.form["submit_button"] == "Yes":
+            try:
+                spotihue = SpotiHue()
+                spotihue.connect_hue_bridge_first_time()
+                return render_template("spotihue_main.html")
+            except:
+                return render_template("spotihue_welcome.html", message='Connection unsuccessful. Please press '
+                                                                        'Hue Bridge button and click/press "Yes".')
+    return render_template("spotihue_main.html")
 
 @app.route("/play", methods=["GET", "POST"])
 def spotihue_sync():
-    if request.form["submit_button"] == "SpotiHue":
-        spotihue = SpotiHue()
-        spotihue.sync_current_track_album_artwork_lights()
-        return render_template("spotihue_main.html", message='SpotiHue was stopped. Turn on Spotify and '
-                                                             'click/press "SpotiHue"')
+    if request.method == "POST":
+        if request.form["submit_button"] == "SpotiHue":
+            spotihue = SpotiHue()
+            spotihue.sync_current_track_album_artwork_lights()
+    return render_template("spotihue_main.html", message='SpotiHue was stopped. Turn on Spotify and '
+                                                         'click/press "SpotiHue"')
 
 if __name__ == "__main__":
     app.run()
