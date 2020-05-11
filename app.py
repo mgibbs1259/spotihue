5b5b45e0bfc2057c3e6059f6434c8102c23e7280
Mary Gibbs <mgibbs1259@gmail.com>
2020-05-11 19:39:49 -0400

    minor refactor

--- app.py
+++ app.py
@@ -1,48 +1,34 @@
 from flask import Flask, render_template, request
-from phue import Bridge
-import spotipy.util as util
-from spotipy import Spotify
 
-import credentials
 from spotihue import SpotiHue
 
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def spotihue_welcome():
     return render_template("spotihue_welcome.html")
 
-
-@app.route("/connect/", methods=["GET", "POST"])
-def spotihue_connection():
+@app.route("/connect", methods=["GET", "POST"])
+def spotihue_connect():
     if request.form["submit_button"] == "Yes":
         try:
-            hue_bridge = Bridge(credentials.hue_bridge_ip_address)
-            hue_bridge.connect()
-            return render_template("spotihue_sync.html", hb_connection_message="Connection Successful")
+            spotihue = SpotiHue()
+            spotihue.connect_hue_bridge_first_time()
+            return render_template("spotihue_main.html")
         except:
-            return render_template("spotihue_welcome.html", hb_connection_message="Connection Unsuccessful. "
-                                                                                  "Please Press Hue Bridge Button "
-                                                                                  "and Try Again.")
+            return render_template("spotihue_welcome.html", message='Connection unsuccessful. Please press Hue Bridge '
+                                                                    'button and click/press "Yes".')
     else:
-        return render_template("spotihue_sync.html", hb_connection_message="Connection Successful")
+        return render_template("spotihue_main.html")
 
-
-@app.route("/sync/", methods=["GET", "POST"])
+@app.route("/play", methods=["GET", "POST"])
 def spotihue_sync():
-    if request.form["submit_button"] == "Sync":
-        hue_bridge = Bridge(credentials.hue_bridge_ip_address)
-        spotify_token = util.prompt_for_user_token(credentials.spotify_username, credentials.spotify_scope,
-                                                   credentials.spotify_client_id, credentials.spotify_client_secret,
-                                                   credentials.spotify_redirect_uri)
-        spotify = Spotify(auth=spotify_token)
-        spotihue = SpotiHue(hue_bridge, spotify)
+    if request.form["submit_button"] == "SpotiHue":
+        spotihue = SpotiHue()
         spotihue.sync_current_track_album_artwork_lights()
-        return render_template("spotihue_sync.html", hb_connection_message="Syncing Stopped. "
-                                                                           "Turn Spotify On and Press Sync.")
-
+        return render_template("spotihue_main.html", message='SpotiHue was stopped. Turn on Spotify and '
+                                                             'click/press "SpotiHue"')
 
 if __name__ == "__main__":
     app.run()
