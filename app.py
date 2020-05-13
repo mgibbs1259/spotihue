8ed91b33dbfd22faa9ba07c6a9d677945673eee3
Mary Gibbs <mgibbs1259@gmail.com>
2020-05-13 19:04:13 -0400

    working flask app

--- app.py
+++ app.py
@@ -6,25 +6,25 @@ from celery import Celery
 from spotihue import SpotiHue
 
 
-flask_app = Flask(__name__)
+app = Flask(__name__)
 
-flask_app.config["SECRET_KEY"] = str(uuid.uuid4())
+app.config["SECRET_KEY"] = str(uuid.uuid4())
 
-flask_app.config["CELERY_BROKER_URL"] = "redis://localhost:6379/0"
-flask_app.config["CELERY_RESULT_BACKEND"] = "redis://localhost:6379/0"
+app.config["CELERY_BROKER_URL"] = "redis://localhost:6379/0"
+app.config["CELERY_RESULT_BACKEND"] = "redis://localhost:6379/0"
 
-celery = Celery(flask_app.name, broker=flask_app.config["CELERY_BROKER_URL"])
-celery.conf.update(flask_app.config)
+celery = Celery(app.name, broker=app.config["CELERY_BROKER_URL"])
+celery.conf.update(app.config)
 
 @celery.task()
 def run_spotihue():
     return SpotiHue().sync_current_track_album_artwork_lights()
 
-@flask_app.route("/")
+@app.route("/")
 def spotihue_welcome():
     return render_template("spotihue_welcome.html")
 
-@flask_app.route("/connect", methods=["GET", "POST"])
+@app.route("/connect", methods=["GET", "POST"])
 def spotihue_connect():
     if request.method == "POST":
         if request.form["submit_button"] == "Yes":
@@ -36,7 +36,7 @@ def spotihue_connect():
                                                                         'Hue Bridge button and click/press "Yes".')
     return render_template("spotihue_main.html")
 
-@flask_app.route("/play", methods=["GET", "POST"])
+@app.route("/play", methods=["GET", "POST"])
 def spotihue_play():
     if request.method == "POST":
         if request.form["submit_button"] == "SpotiHue":
@@ -44,7 +44,7 @@ def spotihue_play():
             session["run_id"] = str(run_id)
             return render_template("spotihue_main.html", message="SpotiHue was started")
 
-@flask_app.route("/stop", methods=["GET", "POST"])
+@app.route("/stop", methods=["GET", "POST"])
 def spotihue_stop():
     if request.method == "POST":
         if request.form["submit_button"] == "Stop SpotiHue":
@@ -54,4 +54,4 @@ def spotihue_stop():
                                                                  'click/press "SpotiHue".')
 
 if __name__ == "__main__":
-    flask_app.run(debug=True)
+    app.run(debug=True)
