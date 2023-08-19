4a9d6844b6b3c8a6301d99913a2a88c7a9bbd475
Mary Gibbs <mgibbs1259@gmail.com>
2023-08-19 18:04:29 +0000

    initial

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">        
-      </header>
+    <div className="app">
+      <div className="app-container">     
+        <h1 className="app-title">spotihue</h1>
+        <button className="start-button">Start</button>
+      </div>
     </div>
   );
 }
