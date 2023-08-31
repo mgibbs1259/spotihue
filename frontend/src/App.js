73edbbde53bbc3302c277695c62882d89fad0d5c
Mary Gibbs <mgibbs1259@gmail.com>
2023-08-31 20:59:06 +0000

    updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
+import GreenBorderButton from './components/GreenBorderButton';
 
 function App() {
+  const handleApiRequest = async (apiEndPoint) => {
+    try {
+      const response = await fetch(apiEndPoint);
+      const data = await response.json();
+      console.log(data); // Handle the API response data
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
     <div className="app">
       <div className="app-container">     
-        <h1 className="app-title">spotihue</h1>
-        <button className="start-button">Start</button>
+        <div>
+          <GreenBorderButton text="configure" fontSize="40px" />
+        </div>
+        <div>
+          <GreenBorderButton text="configure" fontSize="40px" />
+        </div>
       </div>
     </div>
   );
