6b55416ad5519c2ecd0d17217fced72397299b47
Mary Gibbs <mgibbs1259@gmail.com>
2023-09-10 16:59:24 +0000

    select lights

---
+++ frontend/src/components/SelectLightButton.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './SelectLightButton.css';
+import lightSelectedImage from '../assets/light-selected.png';
+import lightDeselectedImage from '../assets/light-deselected.png';
+import lightErrorImage from '../assets/light-error.png';
+
+const SelectLightButton = ({ lightName, onClick }) => {
+    const hasLightName = !!lightName;
+
+    const [selected, setSelected] = useState(false);
+  
+    const buttonClasses = [
+        'select-light-button',
+        hasLightName ? (selected ? 'select-light-button-clicked' : 'select-light-button-not-clicked'): 'select-light-button-no-lights',
+      ].join(' ');
+    
+    const handleButtonClick = () => {
+        setSelected(!selected);
+        if (onClick) {
+            onClick();
+          }
+    };
+
+    const imageSource = hasLightName ? (selected ? lightSelectedImage : lightDeselectedImage): lightErrorImage;
+    
+    return (
+        <div className="select-light-button-container">
+            <img src={imageSource} alt="light bulb" className="light-bulb-image" />
+            <button
+                className={buttonClasses}
+                onClick={handleButtonClick}
+                disabled={!hasLightName}
+            >
+                {hasLightName ? (selected ? `${lightName}` : `${lightName}`) : 'no lights available'}
+            </button>
+        </div>
+    );
+  };
+  
+  SelectLightButton.propTypes = {
+    lightName: PropTypes.string.isRequired,
+    onClick: PropTypes.func, // Optional click handler
+  };
+  
+  export default SelectLightButton;
