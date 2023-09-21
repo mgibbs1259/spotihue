eb6577b4aaf54d57da86d33da9e909a6b949c763
Camille <142921874+BeeStag@users.noreply.github.com>
2023-09-21 09:36:32 -0400

    Improve task handing (#12)
    * Improve task handling
    
    * make task (starting and terminating) idempotent
    * add redis backend and statedb file to celery worker (to accomplish the above)
    * count/use currently-playing track even if it's paused
    * cut calls to Spotify currently-playing in half
    
    * update gitignore
    
    * up current_track_retries, add missing arg to docstring

--- backend/spotihue/constants.py
+++ backend/spotihue/constants.py
@@ -1,3 +1,4 @@
 REDIS_SPOTIFY_ACCESS_TOKEN_KEY = "spotify_auth_token"
 REDIS_TRACK_INFORMATION_KEY = "current_track_information"
 REDIS_SELECTED_LIGHTS_KEY = "lights"
+REDIS_SPOTIHUE_TASK_ID = "spotihue_task_id"
